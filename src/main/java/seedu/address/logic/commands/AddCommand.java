9c07e804afc85bd35154805217a86c67a4aec390
swxsw <suwei9@gmail.com>
2016-10-05 01:32:56 +0800

    Edited AddCommand Constructor parameters
    Edited AddCommand Constructor parameters

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -30,7 +30,8 @@ public class AddCommand extends Command {
      *
      * @throws IllegalValueException if any of the raw values are invalid
      */
-    public AddCommand(String name, String phone, String email, String address, Set<String> tags)
+    public AddCommand(String name, String time, String date, String length, String recurring, 
+                      String priority, String information, Set<String> tags)
             throws IllegalValueException {
         final Set<Tag> tagSet = new HashSet<>();
         for (String tagName : tags) {
@@ -38,9 +39,9 @@ public class AddCommand extends Command {
         }
         this.toAdd = new Task(
                 new Name(name),
-                new Phone(phone),
-                new Email(email),
-                new Address(address),
+                new Phone(time),
+                new Email(date),
+                new Address(length),
                 new UniqueTagList(tagSet)
         );
     }
