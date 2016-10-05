0ecce7a393bd92bbcfe3a366a639972a89b191d1
Jamos Tay <jamostay94@hotmail.com>
2016-10-05 21:01:15 +0800

    Refactored tests

--- src/test/java/seedu/address/logic/parser/CommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/CommandParserTest.java
@@ -23,160 +23,107 @@ public class CommandParserTest {
     @Test
     public void oneValue() {
         ParsedCommand command = new CommandParser("test value");
-        try {
-            assertEquals(command.getValue(), "value");
-        } catch (IllegalValueException e) {
-            fail("Cannot get 1 value.");
-        }
+        assertValue(command, 0, "value");
     }
     
     @Test
     public void multipleValues() {
         ParsedCommand command = new CommandParser("test value1 value2 value3");
-        try {
-            assertEquals(command.getValue(), "value1");
-            assertEquals(command.getValue(0), "value1");
-            assertEquals(command.getValue(1), "value2");
-            assertEquals(command.getValue(2), "value3");
-        } catch (IllegalValueException e) {
-            fail("Cannot get 1 value.");
-        }
+        assertValue(command, 0, "value1");
+        assertValue(command, 0, "value1");
+        assertValue(command, 1, "value2");
+        assertValue(command, 2, "value3");
     }
     
     @Test
     public void oneParamNoValue() {
         ParsedCommand command = new CommandParser("test param/");
-        try {
-            checkNoValues(command);
-            assertEquals(command.getParam("param"), "");
-        } catch (IllegalValueException e) {
-            fail(e.getMessage());
-        }
+        assert(!command.getAllValues().hasNext());
+        assertParam(command, "param", "");
     }
     
     @Test
     public void oneParamNoName() {
         ParsedCommand command = new CommandParser("test /paramValue");
-        try {
-            checkNoValues(command);
-            assertEquals(command.getParam(""), "paramValue");
-        } catch (IllegalValueException e) {
-            fail(e.getMessage());
-        }
+        assertParam(command, "", "paramValue");
     }
     
     @Test
     public void oneParam() {
         ParsedCommand command = new CommandParser("test param/paramValue");
-        try {
-            checkNoValues(command);
-            assertEquals(command.getParam("param"), "paramValue");
-        } catch (IllegalValueException e) {
-            fail(e.getMessage());
-        }
+        assertParam(command, "param", "paramValue");
     }
 
     @Test
     public void emptyParam() {
         ParsedCommand command = new CommandParser("test /");
-        try {
-            checkNoValues(command);
-            assertEquals(command.getParam(""), "");
-        } catch (IllegalValueException e) {
-            fail(e.getMessage());
-        }
+        assertParam(command, "", "");
     }
     
     @Test
     public void spacedParam() {
         ParsedCommand command = new CommandParser("test param/param value");
-        try {
-            checkNoValues(command);
-            assertEquals(command.getParam("param"), "param value");
-        } catch (IllegalValueException e) {
-            fail(e.getMessage());
-        }
+        assertParam(command, "param", "param value");
     }
 
     @Test
     public void multipleParamDelimiters() {
         ParsedCommand command = new CommandParser("test param/param/value");
-        try {
-            checkNoValues(command);
-            assertEquals(command.getParam("param"), "param/value");
-        } catch (IllegalValueException e) {
-            fail(e.getMessage());
-        }
+        assertParam(command, "param", "param/value");
     }
     
     @Test
     public void leadingSpaceParam() {
         ParsedCommand command = new CommandParser("test param/ value");
-        try {
-            checkNoValues(command);
-            assertEquals(command.getParam("param"), " value");
-        } catch (IllegalValueException e) {
-            fail(e.getMessage());
-        }
+        assertParam(command, "param", " value");
     }
     
     
     @Test
     public void multipleParams() {
         ParsedCommand command = new CommandParser("test param1/paramValue1 param2/paramValue2");
-        try {
-            checkNoValues(command);
-            assertEquals(command.getParam("param1"), "paramValue1");
-            assertEquals(command.getParam("param2"), "paramValue2");
-        } catch (IllegalValueException e) {
-            fail(e.getMessage());
-        }
+        assertParam(command, "param1", "paramValue1");
+        assertParam(command, "param2", "paramValue2");
     }
 
     @Test
     public void multipleSpacedParams() {
         ParsedCommand command = new CommandParser("test param1/param Value 1 param2/param Value 2");
-        try {
-            checkNoValues(command);
-            assertEquals(command.getParam("param1"), "param Value 1");
-            assertEquals(command.getParam("param2"), "param Value 2");
-        } catch (IllegalValueException e) {
-            fail(e.getMessage());
-        }
+        assertParam(command, "param1", "param Value 1");
+        assertParam(command, "param2", "param Value 2");
     }
     
 
     @Test
     public void valueAndParam() {
         ParsedCommand command = new CommandParser("test value param/paramValue");
-        try {
-            assertEquals(command.getValue(), "value");
-            assertEquals(command.getParam("param"), "paramValue");
-        } catch (IllegalValueException e) {
-            fail(e.getMessage());
-        }
+        assertValue(command, 0, "value");
+        assertParam(command, "param", "paramValue");
     }
 
     @Test
     public void multipleValuseAndParams() {
         ParsedCommand command = new CommandParser("test value1 value2 param1/param Value 1 param2/param Value 2");
+        assertValue(command, 0, "value1");
+        assertValue(command, 0, "value1");
+        assertValue(command, 1, "value2");
+        assertParam(command, "param1", "param Value 1");
+        assertParam(command, "param2", "param Value 2");
+    }
+    
+    private void assertValue(ParsedCommand command, int index, String value){
         try {
-            assertEquals(command.getValue(), "value1");
-            assertEquals(command.getValue(0), "value1");
-            assertEquals(command.getValue(1), "value2");
-            assertEquals(command.getParam("param1"), "param Value 1");
-            assertEquals(command.getParam("param2"), "param Value 2");
+            assertEquals(command.getValue(index), value);
         } catch (IllegalValueException e) {
             fail(e.getMessage());
         }
     }
     
-    private void checkNoValues(ParsedCommand command){
-        try{
-            command.getValue();
-            fail("Command contained values, expected none.");
-        }catch(IllegalValueException ex){
-            assertEquals(ex.getMessage(), String.format(CommandParser.VALUE_OUT_OF_BOUNDS_MESSAGE, 0));
+    private void assertParam(ParsedCommand command, String param, String paramValue){
+        try {
+            assertEquals(command.getParam(param), paramValue);
+        } catch (IllegalValueException e) {
+            fail(e.getMessage());
         }
     }
 }
