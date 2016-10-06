7a2f86933fb445803e6401150a6dda7a78d0d799
swxsw <suwei9@gmail.com>
2016-10-07 01:12:17 +0800

    Updated Priority class with appropriate methods and  variables
    Updated Priority class with appropriate methods and  variables
    Uses enum to validate argument

--- src/main/java/seedu/address/model/task/Priority.java
+++ src/main/java/seedu/address/model/task/Priority.java
@@ -2,35 +2,68 @@ package seedu.address.model.task;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 
+/**
+ * 
+ * Represents priority of a Task or DatedTask object in task book.
+ * priority stored as acceptedPriority enum.
+ * Guarantees: immutable;
+ */
 public class Priority {
     
-    public final int priority;
-    public static final String TASK_PRIORITY_CONSTRAINTS;
-    public static final String PRIORITY_VALIDATION_REGEX;
+    public final acceptedPriority priority;
+    
+    public static final String TASK_PRIORITY_CONSTRAINTS = "Task priority should either veryhigh, high, medium, low or verylow./n"
+                                                            + "It is case insensitive";
     
     
     
     private enum acceptedPriority{
-        VeryHigh(5), High(4), Medium(3), Low(2), VeryLow(1);
+        VERYHIGH(5), HIGH(4), MEDIUM(3), LOW(2), VERYLOW(1);
+        
         private int enumPriority;
-        acceptedPriority(int value){
-            enumPriority = value;
+        
+        private acceptedPriority(int value){
+            this.enumPriority = value;
         }
-        int getEnumPriority(){
+        public int getEnumPriority(){
             return enumPriority;
+        }
+        
+        public static boolean Contains(String test){
+            for(acceptedPriority enumValue : acceptedPriority.values()){
+                if(enumValue.name() == test){
+                    return true;
+                }
+            }
+            return false;
         }
     }
 
     public Priority(String priority) throws IllegalValueException{
         assert priority != null;
-        this.priority = Integer.parseInt(priority);
-        try{
-            
-        } catch{
-            
+        String upperPriority = priority.toUpperCase();
+        if(acceptedPriority.Contains(upperPriority)){
+            this.priority = acceptedPriority.valueOf(upperPriority);
+        } else {
+            throw new IllegalValueException(TASK_PRIORITY_CONSTRAINTS);
         }
-        
-        
+    }
+    
+    @Override
+    public String toString() {
+        return this.priority.name();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Priority // instanceof handles nulls
+                && this.priority.name().equals(((Priority) other).priority.name())); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return this.priority.name().hashCode();
     }
 
 }
