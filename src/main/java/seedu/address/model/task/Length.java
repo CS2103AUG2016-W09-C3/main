9dbf7cffcfc1ea7f58d657663701508c74c0dfbe
swxsw <suwei9@gmail.com>
2016-10-07 01:43:55 +0800

    Added appropriate methods and variables to Length class
    Added appropriate methods and variables to Length class

--- src/main/java/seedu/address/model/task/Length.java
+++ src/main/java/seedu/address/model/task/Length.java
@@ -3,9 +3,45 @@ package seedu.address.model.task;
 import seedu.address.commons.exceptions.IllegalValueException;
 
 public class Length {
+    
+    public static final String MESSAGE_LENGTH_CONSTRAINTS = "Task length should be alphanumeric characters, "
+                                                            + "used to denote number of hours.";
+    
+    public static final String LENGTH_VALIDATION_REGEX = "\\d+";
+    
+    public final int length;
 
-    public Length(String length) {
-        // TODO Auto-generated constructor stub
+    public Length(String givenLength) throws IllegalValueException {
+        assert givenLength != null;
+        givenLength = givenLength.trim();
+        
+        if (!isValidLength(givenLength)) {
+            throw new IllegalValueException(MESSAGE_LENGTH_CONSTRAINTS);
+        }
+        int numLength = Integer.parseInt(givenLength);
+        this.length = numLength;
+    }
+
+    private boolean isValidLength(String test) {
+        return test.matches(LENGTH_VALIDATION_REGEX);
+    }
+    
+    @Override
+    public String toString() {
+        return this.length + "";
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Length // instanceof handles nulls
+                && this.length == (((Length) other).length)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        Integer tempLength = (Integer) this.length;
+        return tempLength.hashCode();
     }
 
 }
