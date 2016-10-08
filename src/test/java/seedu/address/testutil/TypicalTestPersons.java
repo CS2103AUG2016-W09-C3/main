6a413e2250ebebd86615a7357435fc45e24b5877
swxsw <suwei9@gmail.com>
2016-10-08 23:32:53 +0800

    Updated TypicalTestPersons class with appropriate test data
    Updated TypicalTestPersons class with appropriate test data

--- src/test/java/seedu/address/testutil/TypicalTestPersons.java
+++ src/test/java/seedu/address/testutil/TypicalTestPersons.java
@@ -9,25 +9,26 @@ import seedu.address.model.task.*;
  */
 public class TypicalTestPersons {
 
-    public static TestTask alice, benson, carl, daniel, elle, fiona, george, hoon, ida;
+    public static TestTask aliceMeeting, breadShopping, lorryMaintainance, danielLunch, fetchElle, 
+                           researchPaper, mumLunch, neiceBirhdayMeal, surveyResults;
 
     public TypicalTestPersons() {
         try {
-            alice =  new PersonBuilder().withName("Alice Pauline").withAddress("123, Jurong West Ave 6, #08-111")
-                    .withEmail("alice@gmail.com").withPhone("85355255")
-                    .withTags("friends").build();
-            benson = new PersonBuilder().withName("Benson Meier").withAddress("311, Clementi Ave 2, #02-25")
-                    .withEmail("johnd@gmail.com").withPhone("98765432")
+            aliceMeeting =  new TaskBuilder().withName("Meeting with Alice Pauline").withPriority("medium")
+                    .withInformation("To pitch business idea").withDoneFlag("Not done")
+                    .withTags("boss").build();
+            breadShopping = new TaskBuilder().withName("Buy 2 packet of bread").withPriority("High")
+                    .withInformation("On shopping trip wth friends").withDoneFlag("Not done")
                     .withTags("owesMoney", "friends").build();
-            carl = new PersonBuilder().withName("Carl Kurz").withPhone("95352563").withEmail("heinz@yahoo.com").withAddress("wall street").build();
-            daniel = new PersonBuilder().withName("Daniel Meier").withPhone("87652533").withEmail("cornelia@google.com").withAddress("10th street").build();
-            elle = new PersonBuilder().withName("Elle Meyer").withPhone("9482224").withEmail("werner@gmail.com").withAddress("michegan ave").build();
-            fiona = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427").withEmail("lydia@gmail.com").withAddress("little tokyo").build();
-            george = new PersonBuilder().withName("George Best").withPhone("9482442").withEmail("anna@google.com").withAddress("4th street").build();
+            lorryMaintainance = new TaskBuilder().withName("Send in lorry for maintainance").withDoneFlag("Done").withInformation("Take 10 month interest loan").withPriority("medium").build();
+            danielLunch = new TaskBuilder().withName("Lunch with Daniel Meier").withDoneFlag("Not done").withInformation("Catch up").withPriority("medium").build();
+            fetchElle = new TaskBuilder().withName("Fetch Elle Meyer from airport").withDoneFlag("Not done").withInformation("Returning flight yet to confirm").withPriority("low").build();
+            researchPaper = new TaskBuilder().withName("Finish off research paper").withDoneFlag("Not done").withInformation("Missing appendix A and B").withPriority("veryHigh").build();
+            mumLunch = new TaskBuilder().withName("Treat mum to lunch").withDoneFlag("Not done").withInformation("To celebrate retirement.").withPriority("VeryHigh").build();
 
             //Manually added
-            hoon = new PersonBuilder().withName("Hoon Meier").withPhone("8482424").withEmail("stefan@mail.com").withAddress("little india").build();
-            ida = new PersonBuilder().withName("Ida Mueller").withPhone("8482131").withEmail("hans@google.com").withAddress("chicago ave").build();
+            neiceBirhdayMeal = new TaskBuilder().withName("Bring neice out for a meal").withDoneFlag("Done").withInformation("Belated birthday treat").withPriority("High").build();
+            surveyResults = new TaskBuilder().withName("Collate customer survey results").withDoneFlag("Not done").withInformation("As soon as possible").withPriority("VeryHigh").build();
         } catch (IllegalValueException e) {
             e.printStackTrace();
             assert false : "not possible";
@@ -37,20 +38,21 @@ public class TypicalTestPersons {
     public static void loadAddressBookWithSampleData(AddressBook ab) {
 
         try {
-            ab.addTask(new Task(alice));
-            ab.addTask(new Task(benson));
-            ab.addTask(new Task(carl));
-            ab.addTask(new Task(daniel));
-            ab.addTask(new Task(elle));
-            ab.addTask(new Task(fiona));
-            ab.addTask(new Task(george));
+            ab.addTask(new Task(aliceMeeting));
+            ab.addTask(new Task(breadShopping));
+            ab.addTask(new Task(lorryMaintainance));
+            ab.addTask(new Task(danielLunch));
+            ab.addTask(new Task(fetchElle));
+            ab.addTask(new Task(researchPaper));
+            ab.addTask(new Task(mumLunch));
         } catch (UniqueTaskList.DuplicateTaskException e) {
             assert false : "not possible";
         }
     }
 
     public TestTask[] getTypicalPersons() {
-        return new TestTask[]{alice, benson, carl, daniel, elle, fiona, george};
+        return new TestTask[]{aliceMeeting, breadShopping, lorryMaintainance, 
+                                danielLunch, fetchElle,researchPaper, mumLunch};
     }
 
     public AddressBook getTypicalAddressBook(){
