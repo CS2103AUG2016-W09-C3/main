4b69ff2d1118c18eb220c6e03a83d4d59959a4e4
swxsw <suwei9@gmail.com>
2016-10-10 22:16:21 +0800

    Added Required_params and Possible_params for CommandParser
    Added Required_params and Possible_params for CommandParser

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -9,6 +9,9 @@ import java.util.Set;
 public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
+    
+    public static final String[] REQUIRED_PARAMS = {};
+    public static final String[] POSSIBLE_PARAMS = {"s"};
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all tasks whose names contain any of "
             + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
