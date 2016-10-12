4f67c9a3bd9f345f7930070309a529692e95ee79
swxsw <suwei9@gmail.com>
2016-10-12 11:34:23 +0800

    Fixed clear command by adding prepare clear command to check for redundant parameters
    Fixed clear command by adding prepare clear command to check for
    redundant parameters

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -9,6 +9,10 @@ public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
     public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+    
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Clears the task list completely. Parameter: clear\n"
+            + "Parameter should not contain anything else other than clear\n"
+            + "Example: " + COMMAND_WORD;
 
     public ClearCommand() {}
 
