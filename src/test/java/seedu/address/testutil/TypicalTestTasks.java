0eee8b4187aa555d81a88c1902b04678789a8db8
swxsw <suwei9@gmail.com>
2016-10-12 17:52:57 +0800

    FIxed done command moving task to last in list after mark done
    FIxed done command moving task to last in list after mark done
    Fixed accompanying tests

--- src/test/java/seedu/address/testutil/TypicalTestTasks.java
+++ src/test/java/seedu/address/testutil/TypicalTestTasks.java
@@ -19,23 +19,42 @@ public class TypicalTestTasks {
             aliceMeeting =  new TaskBuilder().withName("Meeting with Alice Pauline").withPriority("medium")
                     .withInformation("To pitch business idea").withDoneFlag("Not done")
                     .withTags("boss").build();
+            
             breadShopping = new TaskBuilder().withName("Buy 2 packet of bread").withPriority("High")
                     .withInformation("On shopping trip wth meier").withDoneFlag("Not done")
                     .withTags("owesMoney", "friends").build();
-            lorryMaintainance = new TaskBuilder().withName("Send in lorry for maintainance").withDoneFlag("Not done").withInformation("Take 10 month interest loan").withPriority("medium").build();
-            danielLunch = new TaskBuilder().withName("Lunch with Daniel Meier").withDoneFlag("Not done").withInformation("Catch up").withPriority("medium").build();
-            fetchElle = new TaskBuilder().withName("Fetch Elle Meyer from airport").withDoneFlag("Not done").withInformation("Returning flight yet to confirm").withPriority("low").build();
-            researchPaper = new TaskBuilder().withName("Finish off research paper").withDoneFlag("Not done").withInformation("Missing appendix A and B").withPriority("veryHigh").build();
-            mumLunch = new TaskBuilder().withName("Treat mum to lunch").withDoneFlag("Not done").withInformation("To celebrate retirement.").withPriority("VeryHigh").build();
+            
+            lorryMaintainance = new TaskBuilder().withName("Send in lorry for maintainance").withDoneFlag("Not done")
+                    .withInformation("Take 10 month interest loan").withPriority("medium").build();
+            
+            danielLunch = new TaskBuilder().withName("Lunch with Daniel Meier").withDoneFlag("Not done")
+                    .withInformation("Catch up").withPriority("medium").build();
+            
+            fetchElle = new TaskBuilder().withName("Fetch Elle Meyer from airport").withDoneFlag("Not done")
+                    .withInformation("Returning flight yet to confirm").withPriority("low").build();
+            
+            researchPaper = new TaskBuilder().withName("Finish off research paper").withDoneFlag("Not done")
+                    .withInformation("Missing appendix A and B").withPriority("veryHigh").build();
+            
+            mumLunch = new TaskBuilder().withName("Treat mum to lunch").withDoneFlag("Not done")
+                    .withInformation("To celebrate retirement.").withPriority("VeryHigh").build();
 
             //Manually added (Not dated)
-            nieceBirthdayMeal = new TaskBuilder().withName("Bring neice out for a meal").withDoneFlag("Not done").withInformation("Belated birthday treat").withPriority("High").build();
-            surveyResults = new TaskBuilder().withName("Collate customer survey results").withDoneFlag("Not done").withInformation("As soon as possible").withPriority("VeryHigh").build();
+            nieceBirthdayMeal = new TaskBuilder().withName("Bring neice out for a meal").withDoneFlag("Not done").
+                    withInformation("Belated birthday treat").withPriority("High").build();
+            
+            surveyResults = new TaskBuilder().withName("Collate customer survey results").withDoneFlag("Not done").
+                    withInformation("As soon as possible").withPriority("VeryHigh").build();
             
             //Manually added (dated)
-            lectureToAttend = new TaskBuilder().withName("Attend CS2103T lecture").withDoneFlag("Not done").withDateTime("10102016", "1400").withLength("2").withRecurrance("5d").withPriority("low").withInformation("There is webcast if you do not attend").build();
-            meetNathan = new TaskBuilder().withName("Meet Nathan for dinner").withDoneFlag("Not done").withDateTime("25112016", "1800").withLength("3").withPriority("medium").withInformation("Meeting nathan to catch up").build();
-            cuttingHair = new TaskBuilder().withName("Cut hair").withDoneFlag("Not done").withDateTime("11102016", "0900").withLength("1").withRecurrance("6d").withPriority("medium").withInformation("Cut hair so your hair will not be long").build();
+            lectureToAttend = new TaskBuilder().withName("Attend CS2103T lecture").withDoneFlag("Not done").withDateTime("10102016", "1400")
+                    .withLength("2").withRecurrance("5d").withPriority("low").withInformation("There is webcast if you do not attend").build();
+            
+            meetNathan = new TaskBuilder().withName("Meet Nathan for dinner").withDoneFlag("Not done").withDateTime("25112016", "1800")
+                    .withLength("3").withPriority("medium").withInformation("Meeting nathan to catch up").build();
+            
+            cuttingHair = new TaskBuilder().withName("Cut hair").withDoneFlag("Not done").withDateTime("11102016", "0900")
+                    .withLength("1").withRecurrance("6d").withPriority("medium").withInformation("Cut hair so your hair will not be long").build();
             
         } catch (IllegalValueException e) {
             e.printStackTrace();
