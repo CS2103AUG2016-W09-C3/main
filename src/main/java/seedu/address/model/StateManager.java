6a739daeb18758442e773283991b5525f285618c
Jamos Tay <jamostay94@hotmail.com>
2016-10-13 16:55:00 +0800

    Create state manager

--- src/main/java/seedu/address/model/StateManager.java
+++ src/main/java/seedu/address/model/StateManager.java
@@ -2,21 +2,34 @@ package seedu.address.model;
 
 import java.util.ArrayList;
 
+import seedu.address.commons.exceptions.StateException;
+
 public class StateManager {
     private ArrayList<AddressBook> states = new ArrayList<>();
     private int currentState = 0;
+    private final String MESSAGE_NO_PREV_STATE = "No previous state to load";
     
-    public StateManager(){
-        
+    public StateManager(AddressBook initialState){
+        states.add(new AddressBook(initialState));
     }
-    
+
     
     public void saveState(AddressBook newState){
-        while(states.size() > currentState){
+        while(states.size() - 1 > currentState){
             states.remove(states.size() - 1);
         }
         states.add(new AddressBook(newState));
         currentState++;
-        
+        System.out.println(currentState);
+    }
+
+
+    public AddressBook loadPreviousState() throws StateException{
+        if(currentState == 0){
+            throw new StateException(MESSAGE_NO_PREV_STATE);
+        }
+        currentState--;
+        System.out.println(currentState);
+        return states.get(currentState);
     }
 }
