51f1b92a9ed59ec0c1c7b1d14f16a09983778a9b
Jamos Tay <jamostay94@hotmail.com>
2016-10-13 16:15:41 +0800

    Create Undo command

---
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -0,0 +1,62 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.tag.UniqueTagList;
+import seedu.address.model.task.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Adds a tasks to the address book.
+ */
+public class UndoCommand extends Command {
+
+    public static final String COMMAND_WORD = "undo";
+
+    public static final String[] REQUIRED_PARAMS = {};
+    public static final String[] POSSIBLE_PARAMS = {""};
+    
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Undoes the previous command. ";
+    public static final String MESSAGE_SUCCESS = "Undid previous command.";
+    public static final String MESSAGE_REDO = "Redid previous command.";
+
+    private final Task toAdd;
+
+    /**
+     * Convenience constructor using raw values for adding 
+     *
+     * @throws IllegalValueException if any of the raw values are invalid
+     */
+    public UndoCommand(){
+    }
+
+    public AddCommand(String name, String priority, String information, String doneFlag, Set<String> tagsFromArgs) 
+            throws IllegalValueException {
+        final Set<Tag> tagSet = new HashSet<>();
+        for (String tagName : tagsFromArgs) {
+            tagSet.add(new Tag(tagName));
+        }
+        this.toAdd = new Task(
+                new Name(name),
+                new Priority(priority),
+                new Information(information),
+                new DoneFlag(doneFlag),
+                new UniqueTagList(tagSet)
+        );
+    }
+
+    @Override
+    public CommandResult execute() {
+        assert model != null;
+        try {
+            model.addTask(toAdd);
+            return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
+        } catch (UniqueTaskList.DuplicateTaskException e) {
+            return new CommandResult(MESSAGE_DUPLICATE_TASK);
+        }
+
+    }
+
+}
