ed2005b367ec06725beb8127ce40eec721d5624b
Jamos Tay <jamostay94@hotmail.com>
2016-10-13 16:54:22 +0800

    Create undo command

--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.commons.exceptions.StateException;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.tag.UniqueTagList;
 import seedu.address.model.task.*;
@@ -22,7 +23,6 @@ public class UndoCommand extends Command {
     public static final String MESSAGE_SUCCESS = "Undid previous command.";
     public static final String MESSAGE_REDO = "Redid previous command.";
 
-    private final Task toAdd;
 
     /**
      * Convenience constructor using raw values for adding 
@@ -32,29 +32,14 @@ public class UndoCommand extends Command {
     public UndoCommand(){
     }
 
-    public AddCommand(String name, String priority, String information, String doneFlag, Set<String> tagsFromArgs) 
-            throws IllegalValueException {
-        final Set<Tag> tagSet = new HashSet<>();
-        for (String tagName : tagsFromArgs) {
-            tagSet.add(new Tag(tagName));
-        }
-        this.toAdd = new Task(
-                new Name(name),
-                new Priority(priority),
-                new Information(information),
-                new DoneFlag(doneFlag),
-                new UniqueTagList(tagSet)
-        );
-    }
-
     @Override
     public CommandResult execute() {
         assert model != null;
         try {
-            model.addTask(toAdd);
-            return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
-        } catch (UniqueTaskList.DuplicateTaskException e) {
-            return new CommandResult(MESSAGE_DUPLICATE_TASK);
+            model.loadPreviousState();
+            return new CommandResult(MESSAGE_SUCCESS);
+        } catch (StateException e) {
+            return new CommandResult(e.getMessage());
         }
 
     }
