dcb5814fb52edd715d5125dd37c35d7e09276b34
Jamos Tay <jamostay94@hotmail.com>
2016-10-19 00:40:14 +0800

    Release v0.3

--- src/main/java/seedu/address/logic/parser/Parser.java
+++ src/main/java/seedu/address/logic/parser/Parser.java
@@ -172,7 +172,6 @@ public class Parser {
             if(command.hasParams(AddCommand.DATED_TASK_PARAMS)){
                 return new AddCommand(
                         command.getValuesAsString(),
-                        command.getParamOrDefault("h", "-1"),
                         command.getParamOrDefault("d", "-1"),
                         command.getParamOrDefault("l", "-1"),
                         command.getParamOrDefault("r", Recurrance.NO_INTERVAL),
@@ -252,7 +251,6 @@ public class Parser {
     		return new EditCommand(
                         Integer.parseInt(command.getValuesAsString()),
                         command.getParamOrDefault("n", ""),
-                        command.getParamOrDefault("h", "-1"),
                         command.getParamOrDefault("d", "-1"),
                         command.getParamOrDefault("l", "-1"),
                         command.getParamOrDefault("r", Recurrance.NO_INTERVAL),
