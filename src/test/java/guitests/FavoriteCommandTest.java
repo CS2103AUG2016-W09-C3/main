d2beed49fef88de1da9813f24e9fe10d14838bd0
Jamos Tay <jamostay94@hotmail.com>
2016-10-28 19:34:23 +0800

    Write some tests for favorite command

---
+++ src/test/java/guitests/FavoriteCommandTest.java
@@ -0,0 +1,65 @@
+package guitests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.FavoriteCommand;
+import seedu.address.testutil.TestTask;
+import seedu.address.testutil.TestUtil;
+
+public class FavoriteCommandTest extends AddressBookGuiTest  {
+
+
+    private final String INVALID_MESSAGE = "Invalid command format! \n" + FavoriteCommand.MESSAGE_USAGE;
+    
+    @Test
+    public void list() {
+        commandBox.runCommand("favorite List c/list");
+        presetListPanel.navigateToPreset("List");
+        TestTask[] expectedList = td.getTypicalTasks();
+        assertTrue(taskListPanel.isListMatching(expectedList));
+    }
+    
+    @Test
+    public void clear() {
+        commandBox.runCommand("favorite Clear c/clear");
+        presetListPanel.navigateToPreset("Clear");
+        TestTask[] expectedList = {};
+        assertTrue(taskListPanel.isListMatching(expectedList));
+    }
+    
+    @Test
+    public void add() {
+        TestTask personToAdd = td.nieceBirthdayMeal;
+        commandBox.runCommand("favorite Add c/" + personToAdd.getAddCommand());
+        presetListPanel.navigateToPreset("Add");
+        TestTask[] expectedList = TestUtil.addTasksToList(td.getTypicalTasks(), personToAdd);
+        assertTrue(taskListPanel.isListMatching(expectedList));
+    }
+    
+    @Test
+    public void multipleCommands() {
+        commandBox.runCommand("favorite Clear c/clear");
+        commandBox.runCommand("favorite Undo c/undo");
+        commandBox.runCommand("favorite Redo c/redo");
+        presetListPanel.navigateToPreset("Clear");
+        TestTask[] expectedList = {};
+        assertTrue(taskListPanel.isListMatching(expectedList));
+        presetListPanel.navigateToPreset("Undo");
+        assertTrue(taskListPanel.isListMatching(td.getTypicalTasks()));
+        presetListPanel.navigateToPreset("Redo");
+        assertTrue(taskListPanel.isListMatching(expectedList));
+    }
+    
+    @Test
+    public void missingParams() {
+        commandBox.runCommand("favorite c/no desc");
+        assertResultMessage(INVALID_MESSAGE);
+        commandBox.runCommand("favorite no command");
+        assertResultMessage(INVALID_MESSAGE);
+    }
+    
+    
+    
+}
